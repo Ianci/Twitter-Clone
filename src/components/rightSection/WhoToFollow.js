2a0563015e8ae19287f2680efe9ae75894f27087
Ianci <ianbergia.rc@gmail.com>
2020-10-19 14:18:20 -0300

    Creacion de array whoToFollow

---
+++ src/components/rightSection/WhoToFollow.js
@@ -0,0 +1,22 @@
+
+import FirebaseImg from '../../images/firebase.png'
+import ReactImg  from '../../images/React-image.png'
+import MaterialUiImg from '../../images/materialUI-photo.jpg'
+
+export const whoToFollow = [
+    {
+        name: 'React',
+        account: '@reactjs',
+        profilePic: ReactImg
+    },
+    {
+        name: 'FirebaseES',
+        account: '@FirebaseES',
+        profilePic: FirebaseImg
+    },
+    {
+        name: 'Material-UI',
+        account: '@MaterialUI',
+        profilePic: MaterialUiImg
+    }
+]
