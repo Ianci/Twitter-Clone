1f011b26c463ce0d45d3745921ee5f41a571d81e
Ianci <ianbergia.rc@gmail.com>
2020-10-19 16:11:34 -0300

    modificaciones varias

--- src/components/rightSection/InputForm.js
+++ src/components/rightSection/InputForm.js
@@ -8,7 +8,9 @@ import {
 
 import InputBase from '@material-ui/core/InputBase';
 import { Timeline } from 'react-twitter-widgets'
-
+import { whoToFollow } from './WhoToFollow'
+import Avatar from '@material-ui/core/Avatar' 
+import Button from '@material-ui/core/Button'
 
 const theme = createMuiTheme({
     palette: {
@@ -36,6 +38,52 @@ const theme = createMuiTheme({
     },
     containerTimeLine: {
       marginTop: theme.spacing(2)
+    },
+    whoToFollow: {
+      border: "1px solid rgba(221,221,221,0.1)",
+      borderRadius: "10px",
+      display: "flex",
+      fleDirection: "column",
+      marginTop: theme.spacing(1)
+    },
+    whoToFollowText: {
+      borderBottom: "1px solid grey",
+      marginBottom: theme.spacing(1),
+      paddingLeft: theme.spacing(1),
+      color: "#fff",
+      fontFamily: "system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Ubuntu, Helvetica Neue, sans-serif",
+      fontSize: '1.2rem',
+      fontWeight: 800,
+      lineHeight: 1.3125
+    },
+    avatarSize: {
+      width: theme.spacing(7),
+      height: theme.spacing(7),
+    },
+    whoToFollowAccounts: {
+      width: '-webkit-fill-available'
+    },
+    whoToFollowContainer: {
+      display: 'flex',
+      justifyContent: 'space-between',
+      borderBottom: '1px solid rgba(221,221,221,0.1)',
+      padding: theme.spacing(2)
+    },
+    whoToFollowAccount: {
+      textAlign: "center",
+      color: "#fff",
+      display: "inline",
+      paddingRight: theme.spacing(4),
+      paddingTop: theme.spacing(1),
+      fontFamily: "system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Ubuntu, Helvetica Neue, sans-serif",
+      fontWeight: 'bold',
+      lineHeight: 1.3125
+    },
+    account: {
+      fontWeight: '100',
+      fontFamily: "system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Ubuntu, Helvetica Neue, sans-serif",
+      color: 'grey'
+
     }
 }));
 
@@ -77,22 +125,60 @@ const InputSearchTwitter = () => {
 
   const classes = useStyles()
   const [ change, setChange ] = useState("reactjs")
+ 
+  const handleChange = e => {
+    if(e.target.value === ''){
+      setChange("reactjs")
+    }
+  }
+
+  useEffect(() => {
     
+  }, [change])
+
   
+   
   return ( 
     <>
+        
          <ThemeProvider theme={theme}>
         <div className={classes.container}>
         
         <StyledInputTwitter placeholder="Buscar en Twitter" 
-         name="input" onChange={e => setChange(e.target.value)}
+         name="input" 
+         onChange={e => setChange(e.target.value)}
+         onBlur={handleChange}
          id="twitter-input"/>
+
           <div className={classes.containerTimeLine}>
-         <Timeline
-          dataSource={{ sourceType: "profile", screenName: `${change}` }}
-          options={{ theme: "dark", width: "300", height: "600", margin: "20px" }}
-        />
-        </div>
+            <Timeline
+            dataSource={{ sourceType: "profile", screenName: `${change}` }}
+            options={{ theme: "dark", width: "320", height: "600", margin: "20px" }}
+            />
+          </div>
+
+          <div className={classes.whoToFollow}>
+           
+            <div className={classes.whoToFollowAccounts}>
+            <p className={classes.whoToFollowText}>A quién seguir</p>
+              {whoToFollow.map((account, index) => {
+                return(
+                  <div className={classes.whoToFollowContainer}>
+                  <Avatar src={account.profilePic} alt="account-profile" className={classes.avatarSize}/>
+                  
+                  <div className={classes.whoToFollowAccount}>
+                  <span>{account.name}</span>
+                  <br/>
+                  <span className={classes.account}>{account.account}</span>
+                  </div>
+                  <Button variant="outlined" color="primary" href={account.href} target="_blank">
+                    Seguir
+                  </Button>
+                  </div>
+                )
+                })}
+            </div>
+          </div>
         </div>
         </ThemeProvider>
         
