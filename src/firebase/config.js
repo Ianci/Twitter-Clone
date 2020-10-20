164f659c26763dc794ecc82621cc3f0458fdb877
Ianci <ianbergia.rc@gmail.com>
2020-10-20 20:12:26 -0300

    Creacion de carpeta y archivos relacionados a Firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBC9pM_O1o6tBqU3xHuhhL1nBtUn7NOmOg",
+    authDomain: "twitter-clone-2034f.firebaseapp.com",
+    databaseURL: "https://twitter-clone-2034f.firebaseio.com",
+    projectId: "twitter-clone-2034f",
+    storageBucket: "twitter-clone-2034f.appspot.com",
+    messagingSenderId: "179566592067",
+    appId: "1:179566592067:web:c3b05d33106a0a353754f6"
+  };
+
+export default firebaseConfig
---
+++ src/firebase/index.js
@@ -0,0 +1,8 @@
+//Index solamente lo vamos a usar para unir el resto de los archivos 
+//y poder importarlo más fácilmente
+
+import firebase from './firebase'
+import FirebaseContext from './firebaseContext'
+
+export { FirebaseContext }
+export default firebase
