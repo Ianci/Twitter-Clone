395ccca125dbb1d8ba9e3b0f7b977055d234a831
Ianci <ianbergia.rc@gmail.com>
2020-10-24 02:27:45 -0300

    useContext import

--- src/components/sidebar/SidebarAvatar.js
+++ src/components/sidebar/SidebarAvatar.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useContext} from 'react';
 import Img2 from '../../images/img2.jpeg'
 import { makeStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
-
+import { FirebaseContext } from '../../firebase/index'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -15,11 +15,17 @@ const useStyles = makeStyles((theme) => ({
   }));
 const SidebarAvatar = () => {
     const classes = useStyles()
+    
+    const { user } = useContext(FirebaseContext)
+
+    //Evitar que throwee error al cargar por primera vez. User arranca null
+    
+    if (!user) return null
     return ( 
         <>
-        <Avatar src={Img2} alt="account-profile" className={classes.root}/>
+        <Avatar src={user.photoURL} alt="account-profile" className={classes.root}/>
         <div className="account-name">
-            <p className="p-name">ianci</p>
+            <p className="p-name">{user.displayName}</p>
             <span className="s-account">@ianbrg11</span>
         </div>
         </>
