3cbff8b3f7b0aaf4aa2647e847318b02c1448638
Ianci <ianbergia.rc@gmail.com>
2020-10-24 02:27:02 -0300

    añadidos nuevos método en la class firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,36 @@
 import app from 'firebase/app'
 import firebaseConfig from './config'
-
+import 'firebase/auth'
+import 'firebase/firestore'
+import 'firebase/storage'
 class Firebase{
     constructor(){
         if(!app.apps.length){
             app.initializeApp(firebaseConfig)
         }
+        this.auth = app.auth();
+        this.db = app.firestore();
+        this.storage = app.storage();
     }
+    //Registrar usuario
+    async accountRegistration(name, email, password){
+    const newUser= await this.auth.createUserWithEmailAndPassword(email, password)
+    
+    return await newUser.user.updateProfile({
+        displayName: name,
+        
+    })
+    
+   
+    }
+    //Login
+    async loginUser(email, password){
+        return await this.auth.signInWithEmailAndPassword(email,password)
+    }
+    async logOut(){
+        return await this.auth.signOut()
+    }
+    
 }
 
 const firebase = new Firebase()
