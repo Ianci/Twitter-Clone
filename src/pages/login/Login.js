7a66beee443719c2e185c5d58aff2855f3818fb8
Ianci <ianbergia.rc@gmail.com>
2020-10-24 02:28:02 -0300

    login y register

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles} from '@material-ui/core/styles';
 import { Formik, Form, ErrorMessage, Field} from 'formik'
 import * as Yup from 'yup';
@@ -6,6 +6,7 @@ import { TextField} from '@material-ui/core'
 import TwitterIcon from '@material-ui/icons/Twitter';
 import TweetLogin from '../../images/TweetLogin.svg'
 import SubmitFormButton from '../register/SubmitFormBtn'
+import { useHistory } from 'react-router-dom'
 //Firebase
 import firebase from '../../firebase'
 
@@ -50,14 +51,44 @@ const useStyles = makeStyles((theme)=>({
         fontWeight: 600,
         lineHeight: 1,
         fontSize: "1rem",
-        padding: "4px"
-
+        padding: "4px",
+        maxWidth: "fit-content",
+        textAlign: "center"
+    },
+    thanksText: {
+        fontWeight: 800,
+        lineHeight: 1.3325,
+        fontSize: "2.4rem",
+        fontFamily: "system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Ubuntu, Helvetica Neue, sans-serif",
+        textAlign: 'center',
+        padding: "0 100px",
+        display: "flex",    
     }
 }))
 
 
 const Login = () => {
     const classes = useStyles()
+    const history = useHistory()
+    const [ error, setError ] = useState(false)
+    const [ isSubmitting, setIsSubmitting] = useState(false)
+
+    async function userLogin(values){
+        try {
+            const user = await firebase.loginUser(values.email, values.password)
+            console.log(user)
+            setIsSubmitting(true)
+            setError(false)
+            setTimeout(() => {
+                history.push('/')  
+            }, 3000);
+        } catch (error) {
+            console.log(error)
+            console.error(error.message)
+            setIsSubmitting(false)
+            setError(error.message)
+        }
+    }
     //Formik and Yup validation
     return ( 
         <Formik initialValues={{email: "", password: ""}}
@@ -69,12 +100,11 @@ const Login = () => {
             .required('Por favor introduce tu contraseña')
         })}
         onSubmit={(values, {setSubmitting}) => {
-           setTimeout(() => {
-
+               setSubmitting(true)
                console.log(JSON.stringify( values, null, 2));
-
+               userLogin(values)
                setSubmitting(false);
-           }, 1000);
+          
         }}>
         {( {isValid, dirty})=>(
             //HTML
@@ -86,6 +116,8 @@ const Login = () => {
                 
                 
                 <div className={classes.containerForm}>
+                    { isSubmitting ? <h1 className={classes.thanksText}>Iniciando sesion...</h1>
+                    :
                     <Form style={{display: "flex", flexDirection: "column"}}>
                     
                     <TwitterIcon style={{height: "50px", width: "50px"}} color="primary"/>
@@ -102,6 +134,8 @@ const Login = () => {
                         Ingrese ahora!
                     </SubmitFormButton>
                     </Form>
+                    }
+                    { error && <p className={classes.errorMessage}>{error}</p>}
                 </div>
             </div>
             
--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles} from '@material-ui/core/styles';
 import { Formik, Form, ErrorMessage, Field} from 'formik'
 import * as Yup from 'yup';
@@ -9,6 +9,11 @@ import AddIcon from '@material-ui/icons/Add';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import TweetRegister from '../../images/TweetRegister.svg'
 import SubmitFormButton from './SubmitFormBtn'
+import FileUploader from 'react-firebase-file-uploader'
+import { useHistory } from 'react-router-dom'
+
+
+
 const useStyles = makeStyles((theme)=>({
     registerContainer: {
         backgroundColor: 'white',
@@ -29,7 +34,8 @@ const useStyles = makeStyles((theme)=>({
     containerForm: {
         margin: "0 auto",
         display: "flex",
-        flexDirection: "column"
+        flexDirection: "column",
+        justifyContent: "center"
     },
     InputForm: {
         marginBottom: theme.spacing(2)
@@ -46,95 +52,126 @@ const useStyles = makeStyles((theme)=>({
         lineHeight: 1.225,
         fontSize: '1rem',
         fontFamily: "system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Ubuntu, Helvetica Neue, sans-serif",
-        padding: '4px'
+        padding: '4px',
+        maxWidth: "fit-content",
+        textAlign: "center"
+    },
+    thanksText: {
+        fontWeight: 800,
+        lineHeight: 1.3325,
+        fontSize: "2.4rem",
+        fontFamily: "system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Ubuntu, Helvetica Neue, sans-serif",
+        textAlign: 'center',
+        padding: "0 100px",
+        display: "flex",
+       
     }
+    
 }))
 
 
 const Register = () => {
     const classes = useStyles()
+    const history = useHistory()
+    const [ error, setError ] = useState(false)
+    const [ errorMsg, setErrorMsg] = useState('')
+    const [ isSubmitting, setIsSubmitting] = useState(false)
 
-  
+    
+        //Register Function
+        async function createAccount(values){
+            try {
+                await firebase.accountRegistration(values.name, values.email, values.password, values.name)
+               
+                setIsSubmitting(true)
+                setError(false)
+                setTimeout(() => {
+                    history.push('/user-dates')
+                }, 1000);
+                
+            } catch (error) {
+               
+                console.error('Se ha producido un error', error.message)
+                setError(true)
+                setIsSubmitting(false)
+                setErrorMsg(error.message)
+            }
+            
+        }
+
     return ( 
         
-        <Formik initialValues={{ name:  "", date: "2020-03-02", email: "", password: "", confirm: ""}}
+        <Formik initialValues={{ name: "", email: "", password: "", confirm: ""}}
         validationSchema= {Yup.object({
+            
             name: Yup.string()
             .required('Por favor introduce tu nombre')
             .max(15, 'Tu nombre debe contener 15 caracteres máximo'),
+
             email: Yup.string()
             .email('Introduce un email válido')
             .required('Completa el campo con tu email'),
-            date: Yup.date()
-            .required('Introduce tu fecha de nacimiento'),
+            
             password: Yup.string()
             .required('Introduce tu contraseña')
             .min(8, 'La contraseña debe tener al menos 8 caracteres')
             .max(15, 'La contraseña debe tener como máximo 15 caracteres'),
+
             confirm: Yup.string()
             .required('Vuelva a escribir la contraseña')
             .oneOf([Yup.ref('password'), null], 'Las contraseñas no coinciden')
         })}
-        onSubmit={(values, { setSubmitting }) => {
-            setTimeout(() => {
-                console.log(JSON.stringify( values, null, 2));
+        onSubmit={(values, {setSubmitting}) => {
+                setSubmitting(true)
+                console.log(values);
+                createAccount(values)
                 setSubmitting(false);
-            }, 1000);
         }}
         >
-            {( { isValid, dirty }) =>(
-
+            {( {isValid, dirty}) =>(
+            
             <div className={classes.registerContainer}>
                     <div className={classes.ImageLeft}>
                         <img src={TweetRegister} alt="twitter-left-img" />
                     </div>
                     <div className={classes.containerForm}>
+                    
+                   
                     <Form style={{display: "flex", flexDirection: "column"}}>
                         <TwitterIcon style={{height: "50px", width: "50px"}} color="primary"/>
                         <h1 className={classes.twitterh1}>Crea su cuenta de Twitter ahora</h1>
-                        <Field as={TextField} type="text" className={classes.InputForm} name="name" label="Nombre" variant="outlined" color="secondary"/>
+                        
+                        <Field as={TextField} type="text" name="name" className={classes.InputForm} label="Tu nombre u apodo" variant="outlined" color="secondary" />
                         <ErrorMessage name="name" component="small" className={classes.errorMessage} />
+
                         <Field as={TextField}  type="email"  className={classes.InputForm} name="email" label="Email" variant="outlined" color="secondary"/>
                         <ErrorMessage name="email" component="small" className={classes.errorMessage} />
-                        <TimePicker />
-
-                        <label htmlFor="upload-photo">
-                        <input
-                            style={{ display: 'none' }}
-                            id="upload-photo"
-                            name="upload-photo"
-                            type="file"
-                        />
-
-                        <Fab
-                            color="secondary"
-                            size="small"
-                            component="span"
-                            aria-label="add"
-                            variant="extended"
-                            style={{margin: '12px'}}
-                        >
-                            <AddIcon /> Añada su foto de perfil
-                        </Fab>
-                        </label>
+                    
 
                         <Field as={TextField} type="password" className={classes.InputForm} name="password" label="Contraseña" variant="outlined" color="secondary"/>
-                        <ErrorMessage name="name" component="small" className={classes.errorMessage} />
+                        <ErrorMessage name="password" component="small" className={classes.errorMessage} />
+
                         <Field as={TextField} type="password" className={classes.InputForm} name="confirm" label="Repite tu contraseña" variant="outlined" color="secondary"/>
                         <ErrorMessage name="confirm" component="small" className={classes.errorMessage} />
                     
                         <SubmitFormButton 
+                        type="submit"
                         disabled={!(isValid && dirty)}
                         >
                             Crea su cuenta ahora!
                         </SubmitFormButton>
                         </Form>
-                    </div>
-            </div>   
+                        
+                        
+                        { error && <p className={classes.errorMessage}> {errorMsg} </p>}
+                        </div>
+                    </div> 
             )}
-        
+            
         </Formik>
+            
      );
 }
  
 export default Register;
+
