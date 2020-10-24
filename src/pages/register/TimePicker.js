096f0dcdacd4bc9e93fd3be2065d22ce40a1bc29
Ianci <ianbergia.rc@gmail.com>
2020-10-24 02:26:38 -0300

    .

--- src/pages/register/TimePicker.js
+++ src/pages/register/TimePicker.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import DateFnsUtils from '@date-io/date-fns';
 import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers'
 
-export default function TimePicker() {
-    const [selectedDate, setSelectedDate] = useState(new Date ('10-22-2020'))
+export default function TimePicker(props) {
+    const [selectedDate, setSelectedDate] = useState(new Date ('1-1-2000'))
 
     const handleDateChange = date => {
         setSelectedDate(date)
@@ -17,7 +17,7 @@ export default function TimePicker() {
             format="MM/dd/yyyy"
             value={selectedDate}
             onChange={handleDateChange}
-            name="date"
+            name={props.name}         
             KeyboardButtonProps={{
                 'aria-label': 'change date',
             }}
