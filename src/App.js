85c65927c406372705c0bf9eac32062c2a516447
Ianci <ianbergia.rc@gmail.com>
2020-10-24 02:25:36 -0300

    Creacion de rutas

--- src/App.js
+++ src/App.js
@@ -3,28 +3,34 @@ import Home from './pages/home/Home'
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Register from './pages/register/Register'
+import UserDates from './pages/register/UserDates'
 import Login from './pages/login/Login'
-
-
+import useAuth  from './hooks/useAuth'
+import Welcome from './pages/welcome/Welcome'
 //Firebase
 import firebase, { FirebaseContext } from './firebase'
 
 
 function App() {
+  const user = useAuth()
   
   return (
 
     <FirebaseContext.Provider 
-    value={{firebase}}
+    value={{
+          firebase,
+          user
+          }}
     >
 
     <div className="app">
       
       <Router>
         <Switch>
-        
+            <Route exact path='/' component={Welcome} />
             <Route path='/register' component={Register} />
-            <Route exact path='/' component={Home} />
+            <Route path='/user-dates' component={UserDates} />
+            <Route path='/home' component={Home} />
             <Route path='/login' component={Login} />
             {["/home", "/explorer", "/notifications", "/messages", "/bookmark", 
           "/lists", "/profile", "/options"].map(path =>(
