182e4330e28c5f5275e063ccd78845d4c851abe0
Ianci <ianbergia.rc@gmail.com>
2020-10-25 14:28:18 -0300

    welcome layout

--- src/pages/welcome/Welcome.js
+++ src/pages/welcome/Welcome.js
@@ -5,11 +5,17 @@ import { Button, Typography } from '@material-ui/core'
 import WelcomeButton from './WelcomeButtons'
 import TwitterIcon from '@material-ui/icons/Twitter';
 import TweetLogin from '../../images/TweetLogin.svg'
+import SearchIcon from '@material-ui/icons/Search';
+import GroupIcon from '@material-ui/icons/Group';
+import ForumIcon from '@material-ui/icons/Forum';
+
 const useStyles = makeStyles((theme)=>({
     welcomeContainer: {
-        background: "#fff",
+        
         minHeight: "100vh",
-        minWidth: "-webkit-fill-available"
+        minWidth: "-webkit-fill-available",
+        background: "#3384B0",
+        background: "linear-gradient(to bottom right, #3384B0, #67529D)"
     },
     container: {
         display: "grid",
@@ -18,31 +24,53 @@ const useStyles = makeStyles((theme)=>({
     },
     left: {
         padding: "20px 20px",
-        
+        position: "relative"
     },
     presentation: {
         fontWeight: 800,
         fontFamily: "system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Ubuntu, Helvetica Neue, sans-serif",
         fontSize: "1.5rem",
         lineHeight: 1.3125,
-        zIndex: 2
+        color: "#fff",
+        textShadow: "4px 2px 2px rgba(0, 0, 0, 0.69)"
     },
     welcomeh1: {
         fontWeight: 800,
         fontFamily: "system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Ubuntu, Helvetica Neue, sans-serif",
         fontSize: "2rem",
-        lineHeight: 1.3125
+        lineHeight: 1.3125,
+        position: "relative",
+        color: "#fff"
+      
     },
     imgContainer:{
-        display: "flex"
+        display: "flex",
+        position: "absolute"
     },
     welcomeImg: {
-
+        maxWidth: "555px",
+        position: "relative"
+    },
+    textWelcome: {
+        position: "relative",
+        top: "40%"
+    },
+    right: {
+        padding: "50px"
+    },
+    spanWelcome: {
+        color: "#fff",
+        fontSize: "1rem",
+        fontWeight: "bold",
+        fontFamily: "system-ui, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Ubuntu, Helvetica Neue, sans-serif",
+    },
+    buttons: {
+        padding: "55px"
     }
 }))
 
 const WelcomePage = () => {
-
+    const history = useHistory()
     const classes = useStyles()
     return ( 
         <div className={classes.welcomeContainer}>
@@ -53,24 +81,27 @@ const WelcomePage = () => {
                        
                     </div>
                     <div className={classes.textWelcome}>
-                        <p className={classes.presentation}>Sigue lo que te interesa</p>
-                        <p className={classes.presentation}>Entérate de que está hablando la gente</p>
-                        <p className={classes.presentation}>Únete a la conversación</p>
+                        <p className={classes.presentation}><SearchIcon/> Sigue lo que te interesa</p>
+                        <p className={classes.presentation}><GroupIcon /> Entérate de que está hablando la gente</p>
+                        <p className={classes.presentation}><ForumIcon /> Únete a la conversación</p>
                     </div>
                       
                 </div>
                 <div className={classes.right}>
                     <TwitterIcon style={{color: "blue"}}/>
                     <h1 className={classes.welcomeh1}>Mirá lo que está pasando en el mundo en este momento</h1>
-                    <span>Únete a twitter hoy mismo</span>
+                    <span style={{position: "relative"}} className={classes.spanWelcome}>Únete a twitter hoy mismo</span>
                     <div className={classes.buttons}>
                         <WelcomeButton
                         variant="contained"
                         color="primary"
+                        onClick={() => history.push('/register')}
                         >Regístrate</WelcomeButton>
+                        
                         <WelcomeButton
                         variant="outlined"
                         color="secondary"
+                        onClick={() => history.push('/login')}
                         >Inicia sesión</WelcomeButton>
                     </div>
                 </div>
